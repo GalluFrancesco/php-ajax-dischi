43472b801ad8f073a64d7942409e89b36a262849
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2022-02-17 14:56:26 +0100

    Add: css Fix: data link img

--- PHP/index.php
+++ PHP/index.php
@@ -13,6 +13,7 @@ require_once __DIR__ .  "/data/data.php";
     <title>Document</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
     <main>
@@ -21,7 +22,7 @@ require_once __DIR__ .  "/data/data.php";
         foreach ($discList as $key=>$value) {
             echo "<div class='disc-card py-3 px-2 mb-4 mx-2'>
                     <div class='img-box mb-2'>
-                        <img class='img-fluid' :src='" .$value["poster"]. "' alt=''>
+                        <img class='img-fluid' src='" .$value["poster"]. "' alt=''>
                     </div>
 
                     <h5>" . strtoupper($value["title"]) . "</h5>
